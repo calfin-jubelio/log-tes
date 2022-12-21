e7236ecdff4b20531400992eed8bdbc132750f7e
calfin-jubelio <macbook@macbook-cal.local>
2022-12-21 13:49:35 +0700

    Setup Package

--- src/modules/enum/logger.ts
+++ src/modules/enum/logger.ts
@@ -2,3 +2,8 @@ export enum LOGGER_STATUS {
   SUCCESS = "SUCCESS",
   FAILED = "FAILED",
 }
+
+export enum ENABLE_LOG{
+  ACTIVE = "active",
+  DEACTIVE = "deactive"
+}
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,8 @@
 import * as dotenv from "dotenv";
+import { ENABLE_LOG } from "../modules/enum/logger";
 dotenv.config();
 export const CONFIG = {
-  ENABLE_LOG: process.env.ENABLE_LOG === undefined ? false : process.env.ENABLE_LOG,
+  ENABLE_LOG: ENABLE_LOG,
   ES_API_KEY: process.env.ES_API_KEY,
   ES_LOG_ID: process.env.ES_LOG_ID,
   ENABLE_APM: process.env.ENABLE_APM === undefined ? false : JSON.parse(process.env.ENABLE_APM),
