0181362bc35102798db6d89833d9ae40e6c2c3cc
calfin-jubelio <macbook@macbook-cal.local>
2022-12-22 22:10:02 +0700

    Setup Package

--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -21,7 +21,7 @@ export const log = async(status:LOGGER_STATUS,activity:string, request:any,respo
       response: response,
       log_date: new Date(),
   }
-  const sql = 'INSERT INTO logs (uuid, status, service,username,activity,request,response,log_date) VALUES ($1, $2, $3, $4,$5,$6,$7) RETURNING *';
+  const sql = 'INSERT INTO logs (uuid, status, service,username,activity,request,response,log_date) VALUES ($1, $2, $3, $4,$5,$6,$7, $8) RETURNING *';
   const values = [payload.uuid,payload.status,payload.service,payload.username,payload.activity,payload.request,payload.response,payload.log_date];
   query(sql, values)
   .then((res) => console.log(res.rows[0]))
